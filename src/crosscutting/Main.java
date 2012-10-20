97aa26605f66ee4ae2adaf08138bf6341ce71347
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2012-10-20 19:53:19 +0400

    commit from kervala

--- src/crosscutting/Main.java
+++ src/crosscutting/Main.java
@@ -10,7 +10,7 @@ import ui.ConsolePresenter;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//asds
 		Scanner scanner = new Scanner(System.in);
 		
 		Presenter presenter = new ConsolePresenter(System.out);
